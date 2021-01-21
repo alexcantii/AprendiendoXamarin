6303f1547f2217ff13fa3b5ec2f54cd7526ec066
alexcantii <alex-el0606@hotmail.es>
2021-01-21 11:25:20 -0500

    first commit

---
+++ Praticascreem/View/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Praticascreem.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new ViewModel.Loginviewmodel();
+        }
+
+
+        private void Registrar_Cliked(object sender, EventArgs e) {
+
+            Navigation.PushAsync(new RegisterPage());
+        
+        }
+    }
+}
